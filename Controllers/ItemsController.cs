ba04d7300e5c8758bad9582998a5845a552cb330
Bin Rohan <binrohan.cs@gmail.com>
2021-07-15 22:08:01 +0600

    Implemented interface for Items, Dto, extensions

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
 using Catalog.Models;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -19,24 +21,27 @@ namespace Catalog.Controllers
 
         // GET /items
         [HttpGet]
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items = _repository.GetItems();
+            var items = _repository
+            .GetItems()
+            .Select( item => item.AsDto());
             return items;
         }
 
         // GET /items/{id}
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id)
+        public ActionResult<ItemDto> GetItem(Guid id)
         {
-            var item = _repository.GetItem(id);
+            var item = _repository
+            .GetItem(id);
 
             if(item is null)
             {
                 return NotFound();
             }
 
-            return Ok(item);
+            return Ok(item.AsDto());
         }
     }
 }
